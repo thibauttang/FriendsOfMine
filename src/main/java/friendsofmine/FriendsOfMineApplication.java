e2ae6d4eb70ae695555917b42f9968541ada13a0
thibauttang <thibaut.tang@gmail.com>
2017-02-27 16:51:17 +0100

    first commit

---
+++ src/main/java/friendsofmine/FriendsOfMineApplication.java
@@ -0,0 +1,12 @@
+package friendsofmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsOfMineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsOfMineApplication.class, args);
+	}
+}
