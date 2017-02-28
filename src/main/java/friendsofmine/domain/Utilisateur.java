fc31669305b7fcaf19316bfaa9fc7fc45a04d2ae
thibauttang <thibaut.tang@gmail.com>
2017-02-28 13:59:26 +0100

    TP1 Fin question 3

---
+++ src/main/java/friendsofmine/domain/Utilisateur.java
@@ -0,0 +1,51 @@
+package friendsofmine.domain;
+
+import org.springframework.stereotype.Controller;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+/**
+ * Created by thibauttang on 28/02/2017.
+ */
+
+@Controller
+public class Utilisateur {
+
+    @NotNull
+    @Size(min=1)
+    private String nom;
+
+    @NotNull
+    @Size(min=1)
+    private String prenom;
+
+    @NotNull
+    @Pattern(regexp="^[a-zA-Z-]+@[a-zA-Z-]+\\.[a-zA-Z]{2,6}$")
+    private String email;
+
+    @NotNull
+    @Pattern(regexp="M|F")
+    private String sexe;
+
+    private Date date;
+
+    public Utilisateur(String nom, String prenom, String email, String sexe) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.sexe = sexe;
+    }
+
+
+    public Utilisateur(String nom, String prenom, String email, String sexe, Date date) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.sexe = sexe;
+        this.date = date;
+    }
+
+}
---
+++ src/test/java/friendsofmine/UtilisateurTest.java
@@ -0,0 +1,105 @@
+package friendsofmine;
+
+import friendsofmine.domain.Utilisateur;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Calendar;
+import java.util.Date;
+
+public class UtilisateurTest {
+
+    private static Validator validator;
+    private Calendar cal;
+    private Date date;
+
+    @BeforeClass
+    public static void setupContext() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Before
+    public void setupData() {
+        cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 1988);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        date = cal.getTime();
+    }
+
+    @Test
+    public void testUtilisateurFemme() {
+        Utilisateur util = new Utilisateur("Dupont", "Jeanne", "jd@jd.com", "F", date);
+        assertTrue(validator.validate(util).size() == 0);
+    }
+
+    @Test
+    public void testUtilisateurHomme() {
+        Utilisateur util = new Utilisateur("Durand", "Jacques", "jd@jd.com", "M", date);
+        assertTrue(validator.validate(util).size() == 0);
+    }
+
+    @Test
+    public void testUtilisateurSansDate() {
+        Utilisateur util = new Utilisateur("Durand", "Jacques", "jd@jd.com", "M");
+        assertTrue(validator.validate(util).size() == 0);
+    }
+
+    @Test
+    public void testUtilisateurNomNull() {
+        Utilisateur util = new Utilisateur(null, "Jacques", "jd@jd.com", "M");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurNomVide() {
+        Utilisateur util = new Utilisateur("", "Jacques", "jd@jd.com", "M");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurPrenomNull() {
+        Utilisateur util = new Utilisateur("Durand", null, "jd@jd.com", "M");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurPrenomVide() {
+        Utilisateur util = new Utilisateur("Durand", "", "jd@jd.com", "M");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurEmailSansArobase() {
+        Utilisateur util = new Utilisateur("Durand", "Eric", "jd.jd.com", "M");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurEmailNull() {
+        Utilisateur util = new Utilisateur("Durand", "Eric", null, "M");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurSexeInvalide() {
+        Utilisateur util = new Utilisateur("Durand", "Eric", "jd@jd.com", "V");
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+    @Test
+    public void testUtilisateurSexeNull() {
+        Utilisateur util = new Utilisateur("Durand", "Eric", "jd@jd.com", null);
+        assertTrue(validator.validate(util).size() > 0);
+    }
+
+}
